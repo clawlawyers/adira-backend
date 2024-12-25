8438174274981f6fdb6b74c2b2d888535669ff9f
Shubham Prajapati <shubham528prajapati@gmail.com>
2024-12-25 18:08:33 +0530

    session removed

--- src/middlewares/auth-middleware.js
+++ src/middlewares/auth-middleware.js
@@ -34,7 +34,6 @@ async function checkUserAuth(req, res, next) {
 
 async function checkClientAuth(req, res, next) {
   try {
-   
     const token = req.headers["authorization"].split(" ")[1];
     if (!token) {
       throw new AppError("Missing jwt token", StatusCodes.BAD_REQUEST);
@@ -52,16 +51,17 @@ async function checkClientAuth(req, res, next) {
     // Find the session and update its activity timestamp
     if (response.phoneNumber == "8603805697") {
       session = client.sessions[0];
-    } else {
-      session = client.sessions.find(
-        (session) => session.sessionId === response.sessionId
-      );
-      if (!session) {
-        return res.status(401).send("Invalid session");
-      }
-
-      session.lastActive = Date.now(); // Update last active time
     }
+    // else {
+    //   session = client.sessions.find(
+    //     (session) => session.sessionId === response.sessionId
+    //   );
+    //   if (!session) {
+    //     return res.status(401).send("Invalid session");
+    //   }
+
+    //   session.lastActive = Date.now(); // Update last active time
+    // }
     await client.save();
 
     console.log(client);
